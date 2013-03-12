01124b78631073bec2746113179a9f1301565ddc
Matteo Spinelli <matteo@cubiq.org>
2013-03-12 19:55:41 +0100

    exceptions added to config.js

--- src/config.js
+++ src/config.js
@@ -18,7 +18,25 @@
 	// Acko
 	config.pages.push({
 		url: '^http://acko.net',
-		element: document.querySelector('.content-holder')
+		element: document.getElementsByClassName('content-holder')[0]
+	});
+
+	// Google calendar
+	config.pages.push({
+		url: '^https://www.google.com/calendar/',
+		element: document.getElementById('scrolltimedeventswk')
+	});
+
+	// Google Drive
+	config.pages.push({
+		url: '^https://drive.google.com',
+		element: document.getElementsByClassName('doclistview-list')[0]
+	});
+
+	// Google Document
+	config.pages.push({
+		url: '^https://docs.google.com/document/d/([a-zA-Z0-9-_])*/view',
+		element: document.getElementsByClassName('kix-appview-editor')[0]
 	});
 
 	window.scrollbench_config = config;
