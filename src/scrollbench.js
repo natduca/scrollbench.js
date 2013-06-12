9aead0f23dc010f2d632bb7b74bdb8896b4211e8
Tom Wiltzius <wiltzius@chromium.org>
2013-06-12 16:01:49 -0700

    fix typo again

--- src/scrollbench.js
+++ src/scrollbench.js
@@ -8,7 +8,7 @@
  */
 
 (function (window, document) {
-	var CONFIG_URL = 'https://raw.github.com/natduca/scrollbench.js/master/src/scrollbench.js';
+	var CONFIG_URL = 'https://raw.github.com/natduca/scrollbench.js/master/src/config.js';
 	var REPORT_URL = '//natduca.github.com/scrollbench.js/report.html';
 
 	var reliabilityReport = {};
